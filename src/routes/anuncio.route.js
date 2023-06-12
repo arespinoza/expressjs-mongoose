55a9f44260df396fec533587549f4565e68993a4
arespinoza <alfredo_espi@hotmail.com>
2023-06-11 23:19:16 -0300

    corrigiendo

---
+++ src/routes/anuncio.route.js
@@ -0,0 +1,18 @@
+//const anuncioCtrl = require("./../controllers/anuncio.controller");
+
+const express = require("express");
+const router = express.Router();
+
+
+//router.get("/", anuncioCtrl.getAnuncios);
+//router.post("/filtro", anuncioCtrl.getAnunciosFiltro);
+//router.get("/:id", anuncioCtrl.getAnuncio);
+//router.post("/", anuncioCtrl.crearAnuncio);
+//router.put("/:id", anuncioCtrl.editarAnuncio);
+//router.delete("/:id", anuncioCtrl.eliminarAnuncio);
+//router.post("/notification", anuncioCtrl.sendNotificationPush);
+
+
+
+module.exports = router;
+
